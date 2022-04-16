651b711c517597dff6745fccd475d05e50d40033
Temirhan <qwe.mister.qwe@bk.ru>
2022-04-16 20:20:00 +0600

    dtf

--- pages/admin/index.php
+++ pages/admin/index.php
@@ -1,6 +1,7 @@
 <?php
     include("../../inc/connect.php");
     $ressult = mysqli_query($connect , "SHOW TABLES FROM `di-driver`");
+    
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -14,29 +15,247 @@
 <body>
     <div class="admin__panel flex">
         <div class="admin__left__menu flex">
-            <?php 
-                $i = 1;
-                foreach($ressult as $product){
-            ?>
-            <div class="left__menu__block"><a href="" class="left__menu__link" id="<?echo $i?>"><? echo $product["Tables_in_di-driver"]?></a></div>
-            <?php 
-            $i++;
-            }
-            ?>
-        </div>
-        <div class="admin__panel__body">
-            <?php 
-                $i = 1;
-                foreach($ressult as $product){
-                $prp = $product['Tables_in_di-driver'];
-                $table = mysqli_query($connect,"SELECT * FROM `$prp`");
-                $table = mysqli_fetch_assoc($table)
-            ?>
-                <div class="element" id="<?echo $i?>"><?print_r($table)?></div>
-            <?php 
-                $i++;
+                <?php 
+                    $i = 1;
+                    foreach($ressult as $product){
+                        $prp = $product['Tables_in_di-driver'];
+                        $tableName = mysqli_query($connect, "SELECT * FROM `tablename` WHERE `nameTable` = '{$prp}'");
+                        $tableName = mysqli_fetch_assoc($tableName);
+                        if($tableName["nameTable"] == $prp){
+                ?>
+                        <div class="left__menu__block"><a href="" class="left__menu__link" id="<?echo $prp?>"><?echo $tableName["nameTableKZ"]?></a></div>    
+                <?php 
                 }
-            ?>
+                    $i++;
+                    }
+                ?>
+        </div>
+        
+        <div class="admin__panel__table">
+            <div class="add__button__block">
+                <a class="add__button__block__a" href="">Добавить</a>
+                <form class="form" action="inc/add.php" method="post">
+
+                </form>
+            </div>
+            <div class="admin__panel__body">
+                <div class="panel__body__table welcome">
+                    <div class="welcome__content">
+                        WELCOME ADMIN PANEL
+                    </div>
+                </div>
+                <div class="panel__body__table" id="breakdown">
+                    <div class="body__table__line body__table__header flex">
+                            <div class="table__header__title table__title">Код</div>
+                            <div class="table__header__title table__title">Название</div>
+                            <div class="table__header__title table__title">Редактировать</div>
+                            <div class="table__header__title table__title">Удалить</div>
+                    </div>
+                    <?php 
+                            $breakdowns = mysqli_query($connect,"SELECT * FROM `breakdown`");
+                            foreach($breakdowns as $breakdown){
+                            ?>
+                                    <div class="body__table__line _table-color flex">
+                                        <div class="table__title"><?echo $breakdown["id"] ?></div>
+                                        <div class="table__title"><?echo $breakdown["name"] ?></div>
+                                        <div class="table__title"><a href="" class="icon-edit"></a></div>
+                                        <div class="table__title"><a href="inc/delete.php?id=<?php echo $breakdown["id"]?>&table=breakdown" class="icon-bin"></a></div>
+                                    </div>       
+                                <?php
+                            }
+                    ?>
+                </div>
+                <div class="panel__body__table" id="cars">
+                    <div class="body__table__line body__table__header flex">
+                            <div class="table__header__title table__title">Код</div>
+                            <div class="table__header__title table__title">Наименование</div>
+                            <div class="table__header__title table__title">Год выпуска</div>
+                            <div class="table__header__title table__title">Новый?</div>
+                            <div class="table__header__title table__title">Редактировать</div>
+                            <div class="table__header__title table__title">Удалить</div>
+                    </div>
+                    <?php 
+                            $breakdowns = mysqli_query($connect,"SELECT * FROM `cars`");
+                            foreach($breakdowns as $breakdown){
+                            ?>
+                                    <div class="body__table__line _table-color flex">
+                                        <div class="table__title"><?echo $breakdown["id"] ?></div>
+                                        <div class="table__title"><?echo $breakdown["name"] ?></div>
+                                        <div class="table__title"><?echo $breakdown["yearRelease"] ?></div>
+                                        <div class="table__title"><?echo $breakdown["new"] ?></div>
+                                        <div class="table__title"><a href="" class="icon-edit"></a></div>
+                                        <div class="table__title"><a href="inc/delete.php?id=<?php echo $breakdown["id"]?>&table=cars" class="icon-bin"></a></div>
+                                    </div>       
+                                <?php
+                            }
+                    ?>
+                </div>
+                <div class="panel__body__table" id="classes">
+                    <div class="body__table__line body__table__header flex">
+                            <div class="table__header__title table__title">Код</div>
+                            <div class="table__header__title table__title">Наименование</div>
+                            <div class="table__header__title table__title">Редактировать</div>
+                            <div class="table__header__title table__title">Удалить</div>
+                    </div>
+                    <?php 
+                            $breakdowns = mysqli_query($connect,"SELECT * FROM `classes`");
+                            foreach($breakdowns as $breakdown){
+                            ?>
+                                    <div class="body__table__line _table-color flex">
+                                        <div class="table__title"><?echo $breakdown["id"] ?></div>
+                                        <div class="table__title"><?echo $breakdown["name"] ?></div>
+                                        <div class="table__title"><a href="" class="icon-edit"></a></div>
+                                        <div class="table__title"><a href="inc/delete.php?id=<?php echo $breakdown["id"]?>&table=classes" class="icon-bin"></a></div>
+                                    </div>       
+                                <?php
+                            }
+                    ?>
+                </div>
+                <div class="panel__body__table" id="components">
+                    <div class="body__table__line body__table__header flex">
+                            <div class="table__header__title table__title">Код</div>
+                            <div class="table__header__title table__title">Наименование</div>
+                            <div class="table__header__title table__title">Редактировать</div>
+                            <div class="table__header__title table__title">Удалить</div>
+                    </div>
+                    <?php 
+                            $breakdowns = mysqli_query($connect,"SELECT * FROM `components`");
+                            foreach($breakdowns as $breakdown){
+                            ?>
+                                    <div class="body__table__line _table-color flex">
+                                        <div class="table__title"><?echo $breakdown["id"] ?></div>
+                                        <div class="table__title"><?echo $breakdown["name"] ?></div>
+                                        <div class="table__title"><a href="" class="icon-edit"></a></div>
+                                        <div class="table__title"><a href="inc/delete.php?id=<?php echo $breakdown["id"]?>&table=components" class="icon-bin"></a></div>
+                                    </div>       
+                                <?php
+                            }
+                    ?>
+                </div>
+                <div class="panel__body__table" id="customers">
+                    <div class="body__table__line body__table__header flex">
+                            <div class="table__header__title table__title">Код</div>
+                            <div class="table__header__title table__title">ФИО</div>
+                            <div class="table__header__title table__title">Паспорт</div>
+                            <div class="table__header__title table__title">Адрес</div>
+                            <div class="table__header__title table__title">Телефон</div>
+                            <div class="table__header__title table__title">Редактировать</div>
+                            <div class="table__header__title table__title">Удалить</div>
+                    </div>
+                    <?php 
+                            $breakdowns = mysqli_query($connect,"SELECT * FROM `customers`");
+                            foreach($breakdowns as $breakdown){
+                            ?>
+                                    <div class="body__table__line _table-color flex">
+                                        <div class="table__title"><?echo $breakdown["id"] ?></div>
+                                        <div class="table__title"><?echo $breakdown["name"] ?></div>
+                                        <div class="table__title"><?echo $breakdown["pasport"] ?></div>
+                                        <div class="table__title"><?echo $breakdown["addres"] ?></div>
+                                        <div class="table__title"><?echo $breakdown["phone"] ?></div>
+                                        <div class="table__title"><a href="" class="icon-edit"></a></div>
+                                        <div class="table__title"><a href="inc/delete.php?id=<?php echo $breakdown["id"]?>&table=customers" class="icon-bin"></a></div>
+                                    </div>       
+                                <?php
+                            }
+                    ?>
+                </div>
+                <div class="panel__body__table" id="orders">
+                    <div class="body__table__line body__table__header flex">
+                            <div class="table__header__title table__title">Код</div>
+                            <div class="table__header__title table__title">ФИО</div>
+                            <div class="table__header__title table__title">Паспорт</div>
+                            <div class="table__header__title table__title">Адрес</div>
+                            <div class="table__header__title table__title">Телефон</div>
+                            <div class="table__header__title table__title">Редактировать</div>
+                            <div class="table__header__title table__title">Удалить</div>
+                    </div>
+                    <?php 
+                            $breakdowns = mysqli_query($connect,"SELECT * FROM `orders`");
+                            foreach($breakdowns as $breakdown){
+                            ?>
+                                    <div class="body__table__line _table-color flex">
+                                        <div class="table__title"><?echo $breakdown["id"] ?></div>
+                                        <div class="table__title"><?echo $breakdown["name"] ?></div>
+                                        <div class="table__title"><?echo $breakdown["pasport"] ?></div>
+                                        <div class="table__title"><?echo $breakdown["addres"] ?></div>
+                                        <div class="table__title"><?echo $breakdown["phone"] ?></div>
+                                        <div class="table__title"><a href="" class="icon-edit"></a></div>
+                                        <div class="table__title"><a href="inc/delete.php?id=<?php echo $breakdown["id"]?>&table=orders" class="icon-bin"></a></div>
+                                    </div>       
+                                <?php
+                            }
+                    ?>
+                </div>
+                <div class="panel__body__table" id="positions">
+                    <div class="body__table__line body__table__header flex">
+                            <div class="table__header__title table__title">Код</div>
+                            <div class="table__header__title table__title">Наименование</div>
+                            <div class="table__header__title table__title">Редактировать</div>
+                            <div class="table__header__title table__title">Удалить</div>
+                    </div>
+                    <?php 
+                            $breakdowns = mysqli_query($connect,"SELECT * FROM `positions`");
+                            foreach($breakdowns as $breakdown){
+                            ?>
+                                    <div class="body__table__line _table-color flex">
+                                        <div class="table__title"><?echo $breakdown["id"] ?></div>
+                                        <div class="table__title"><?echo $breakdown["name"] ?></div>
+                                        <div class="table__title"><a href="" class="icon-edit"></a></div>
+                                        <div class="table__title"><a href="inc/delete.php?id=<?php echo $breakdown["id"]?>&table=positions" class="icon-bin"></a></div>
+                                    </div>       
+                                <?php
+                            }
+                    ?>
+                </div>
+                <div class="panel__body__table" id="reason">
+                    <div class="body__table__line body__table__header flex">
+                            <div class="table__header__title table__title">Код</div>
+                            <div class="table__header__title table__title">Наименование</div>
+                            <div class="table__header__title table__title">Редактировать</div>
+                            <div class="table__header__title table__title">Удалить</div>
+                    </div>
+                    <?php 
+                            $breakdowns = mysqli_query($connect,"SELECT * FROM `reason`");
+                            foreach($breakdowns as $breakdown){
+                            ?>
+                                    <div class="body__table__line _table-color flex">
+                                        <div class="table__title"><?echo $breakdown["id"] ?></div>
+                                        <div class="table__title"><?echo $breakdown["name"] ?></div>
+                                        <div class="table__title"><a href="" class="icon-edit"></a></div>
+                                        <div class="table__title"><a href="inc/delete.php?id=<?php echo $breakdown["id"]?>&table=reason" class="icon-bin"></a></div>
+                                    </div>       
+                                <?php
+                            }
+                    ?>
+                </div>
+                <div class="panel__body__table" id="staff">
+                    <div class="body__table__line body__table__header flex">
+                            <div class="table__header__title table__title">Код</div>
+                            <div class="table__header__title table__title">ФИО</div>
+                            <div class="table__header__title table__title">Паспорт</div>
+                            <div class="table__header__title table__title">Адрес</div>
+                            <div class="table__header__title table__title">Телефон</div>
+                            <div class="table__header__title table__title">Редактировать</div>
+                            <div class="table__header__title table__title">Удалить</div>
+                    </div>
+                    <?php 
+                            $breakdowns = mysqli_query($connect,"SELECT * FROM `staff`");
+                            foreach($breakdowns as $breakdown){
+                            ?>
+                                    <div class="body__table__line _table-color flex">
+                                        <div class="table__title"><?echo $breakdown["id"] ?></div>
+                                        <div class="table__title"><?echo $breakdown["name"] ?></div>
+                                        <div class="table__title"><?echo $breakdown["pasport"] ?></div>
+                                        <div class="table__title"><?echo $breakdown["addres"] ?></div>
+                                        <div class="table__title"><?echo $breakdown["phone"] ?></div>
+                                        <div class="table__title"><a href="" class="icon-edit"></a></div>
+                                        <div class="table__title"><a href="inc/delete.php?id=<?php echo $breakdown["id"]?>&table=staff" class="icon-bin"></a></div>
+                                    </div>       
+                                <?php
+                            }
+                    ?>
+                </div>
+            </div>
         </div>
     </div>
     <script src="js/script.js"></script>
--- pages/admin/js/script.js
+++ pages/admin/js/script.js
@@ -1,15 +1,39 @@
 const links = document.querySelectorAll(".left__menu__link")
-const content = document.querySelectorAll(".element")
+const content = document.querySelectorAll(".panel__body__table")
+const addButton = document.querySelector(".add__button__block__a")
 links.forEach(element =>{
     element.addEventListener("click", (e) => {
         e.preventDefault()
         links.forEach(el =>{el.classList.remove("active")})
-        e.target.classList.add("active")
-        content.forEach(el =>{el.classList.remove("element-active")})
+        content.forEach(el =>{el.style.display="none"})
         content.forEach(el =>{
             if(e.target.id === el.id){
-                el.classList.add("element-active")
+                e.target.classList.add("active")
+                el.style.display="block"
+                addButton.closest(".add__button__block").style.display="block"
+                addButton.addEventListener("click",(e) =>{
+                    e.preventDefault()
+                    const addButtons = e.target.closest(".add__button__block")
+                    const block = addButtons.querySelector(".form")
+                    console.log(el.children)
+                    let childs = el.children
+                    let childss = childs[0].children
+                    let childss1 = Array.from(childss)
+                    childss1.shift()
+                    childss1.pop()
+                    childss1.pop()
+                    console.log(childss1)
+                    block.insertAdjacentHTML("beforeEnd",`<input name="table" value="${el.id}" style="display:none;"/>`)
+                    childss1.forEach(element => {
+                        console.log(element.innerHTML)
+                        block.append(element.innerHTML)
+                        block.insertAdjacentHTML("beforeEnd",`<input name="item[]"/>`)
+                        block.style.display="block"
+                    })
+                    block.insertAdjacentHTML("beforeEnd",`<button type="submit">Добавить</button>`)
+                })
             }
         })
     })
 })
+
