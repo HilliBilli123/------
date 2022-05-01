32e6aea78f271b3e52867c36f89f06bdba106269
Temirhan <qwe.mister.qwe@bk.ru>
2022-05-01 13:15:39 +0600

    232131

--- pages/admin/inc/edit.php
+++ pages/admin/inc/edit.php
@@ -1,5 +1,33 @@
 <?php
-
+session_start();
+$id = $_POST["id"];
+echo $id;
+$table = $_POST["table"];
+$name = $_POST["name"];
+$yearRelease = $_POST["yearRelease"];
+$new = $_POST["new"];
+$price = $_POST["price"];
 include "../../../inc/connect.php";
-mysqli_query($connect, "UPDATE `repair` SET `dateEnd`='$date' WHERE `id`='$id'");
-header('Location: ../index.php');
+// $break = mysqli_query($connect, "SELECT * FROM `cars` WHERE `id` = '$id'");
+// print_r($break);
+if ($table == "cars") {
+	if ($_FILES['image']['name']) {
+		// echo "323123123";
+		$path = 'upload/' . time() . $_FILES['image']['name'];
+		if (!move_uploaded_file($_FILES['image']['tmp_name'], "../" . $path)) {
+			$_SESSION['message'] = 'Ошибка при загрузке фото';
+			header('Location: ../index.php');
+		}
+		mysqli_query($connect, "UPDATE `cars` SET `name` = '$name',`yearRelease` = '$yearRelease',`new` = '$new',`price` = '$price', `imagePath` = '$path' WHERE `id` = '$id'");
+	} else {
+		$_SESSION['message'] = 'Загрузите фото';
+		header("Location: ../index.php");
+	}
+}
+// echo $path;
+if ($table == "breakdown") {
+	mysqli_query($connect, "UPDATE $table SET `name` = '$name' WHERE `id`='$id'");
+}
+
+// mysqli_query($connect, "UPDATE `repair` SET `dateEnd`='$date' WHERE `id`='$id'");
+// header('Location: ../index.php');
